7f967978bafdc5566e04786d1ebd20a4628517a7
elad-ivanir <eladivanir64@gmail.com>
2020-03-31 20:37:03 +0300

    positions

--- Public/index.js
+++ Public/index.js
@@ -4,15 +4,17 @@ function to_ul(obj) {
   // --------v create an <ul> element
   let f, li;
   const ul = document.createElement("ul");
+  ul.className = "tree__list";
 
   // --v loop through its children
   for (let child of obj.children) {
     li = document.createElement("li");
+    li.className = "tree__list__item";
 
-    let childHtml;
+    let childHtml = document.createElement("a");
     // if the child has a 'folder' prop on its own, call me again
     if (child.children) {
-      childHtml = document.createElement("button");
+      childHtml.className = "tree__list__item--withChildren";
       childHtml.appendChild(document.createTextNode(child.name));
       childHtml.onclick = function() {
         this.parentElement.children[1].hidden = !this.parentElement.children[1]
@@ -21,7 +23,6 @@ function to_ul(obj) {
       li.appendChild(childHtml);
       li.appendChild(to_ul(child));
     } else {
-      childHtml = document.createElement("a");
       childHtml.appendChild(document.createTextNode(child.name));
       childHtml.href = `?file_to_play=${child.fullPath}`;
       li.appendChild(childHtml);
@@ -39,17 +40,17 @@ let readyPromise = new Promise(res => (resolvePromise = res));
 window.addEventListener("DOMContentLoaded", async event => {
   const responseBody = await fetch("./directory");
 
-  const container = document.getElementById("container");
+  const swfPlayerContainer = document.getElementById("swfPlayerContainer");
 
   const ruffle = window.RufflePlayer.newest();
   rufflePlayer = ruffle.create_player();
-  container.innerHTML = "";
-  container.appendChild(rufflePlayer);
+  swfPlayerContainer.innerHTML = "";
+  swfPlayerContainer.appendChild(rufflePlayer);
 
-  const rightContainer = document.getElementById("right_container");
+  const fileTreeContainer = document.getElementById("fileTreeContainer");
   const uls = to_ul(await responseBody.json());
   uls.hidden = false;
-  rightContainer.appendChild(uls);
+  fileTreeContainer.appendChild(uls);
   resolvePromise();
 });
 
